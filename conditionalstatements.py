2accd827cb5f9354987654643e34b4974168bc87
amarjee-tk <116877678+amarjee-tk@users.noreply.github.com>
2023-09-11 12:14:34 +0530

    Add files via upload

---
+++ conditionalstatements.py
@@ -0,0 +1,19 @@
+# age = 17.9
+# if age < 18:
+#     print("You are a minor.")
+# elif age == 18:
+#     print("You just became an adult!")
+# else:
+#     print("You are an adult.")
+    
+    
+    
+    
+age = 1
+if age < 18:
+    print("You are a minor.")
+elif age >= 18:
+    print("You are an adult.")
+else:
+    print("Invalid age.")
+
